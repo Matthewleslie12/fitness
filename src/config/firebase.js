65a0b7f903254c76d44eec062e32da9f9c6044c7
Matthew Leslie <matthewleslie155@gmail.com>
2023-10-04 17:35:26 -0400

    added dotenv

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -11,13 +11,13 @@ import {
 import {initializeApp} from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBOt54wznk4AJewUXineOP638ghRCfiQBs",
-  authDomain: "fitness-app-f1866.firebaseapp.com",
-  projectId: "fitness-app-f1866",
-  storageBucket: "fitness-app-f1866.appspot.com",
-  messagingSenderId: "423175698889",
-  appId: "1:423175698889:web:20b2bf71992f153a791018",
-  measurementId: "G-JDLYLRN20G",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
